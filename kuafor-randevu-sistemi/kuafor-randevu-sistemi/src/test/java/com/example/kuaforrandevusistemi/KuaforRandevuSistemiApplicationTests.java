e9ac9d936bae4137a86cc1f0eddeca6506a6be91
orhungo <131930072@ogr.uludag.edu.tr>
2023-12-20 18:05:35 +0300

    updated

---
+++ kuafor-randevu-sistemi/kuafor-randevu-sistemi/src/test/java/com/example/kuaforrandevusistemi/KuaforRandevuSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kuaforrandevusistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuaforRandevuSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
