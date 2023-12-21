c896e7e239122623f867a179dc978e09c38fe55c
Simge Mede <109466550+simgemede@users.noreply.github.com>
2023-12-21 14:37:29 +0300

    create kullanici.java
    entity klasörü altında kayıt işlemi için oluşturuldu.

---
+++ kuafor-randevu-sistemi/kuafor-randevu-sistemi/src/main/java/com/example/kuaforrandevusistemi/entity/kullanici.java
@@ -0,0 +1,12 @@
+package com.example.kuaforrandevusistemi.entity;
+
+public class kullanici {
+
+    private Long id; // her kayıt olan kişi için id belirlenecek
+    private String ad;
+    private String soyad;
+    private String email;
+    private String telno;
+    private String sifre;
+
+}
