fb5195f1541e7077f817be1ab90050750b7466a4
Simge Mede <109466550+simgemede@users.noreply.github.com>
2023-12-25 15:03:04 +0300

    .

---
+++ kuafor-randevu-sistemi/kuafor-randevu-sistemi/src/main/java/com/example/kuaforrandevusistemi/service/kullaniciService.java
@@ -0,0 +1,9 @@
+package com.example.kuaforrandevusistemi.service;
+
+import com.example.kuaforrandevusistemi.dto.kullaniciDTO;
+
+public interface kullaniciService {
+
+    kullaniciDTO kullaniciDTO(kullaniciDTO kullaniciDTO);
+
+}
